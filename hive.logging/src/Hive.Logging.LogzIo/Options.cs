bae697e47a8a54d4787fceb91046587e26db1629
Maciek Misztal <mmisztal1980@gmail.com>
2024-02-18 23:25:45 +0100

    fix: Hive.Logging.LogzIo formatting

--- hive.logging/src/Hive.Logging.LogzIo/Options.cs
+++ hive.logging/src/Hive.Logging.LogzIo/Options.cs
@@ -1,16 +1,22 @@
 ﻿namespace Hive.Logging.LogzIo;
 
+/// <summary>
+/// Options for LogzIo.
+/// </summary>
 public class Options
 {
-    public const string SectionKey = "Hive:Logging:LogzIo";
+  /// <summary>
+  /// The configuration section key.
+  /// </summary>
+  public const string SectionKey = "Hive:Logging:LogzIo";
 
-    /// <summary>
-    /// LogzIo Token
-    /// </summary>
-    public string Token { get; set; } = default!;
+  /// <summary>
+  /// LogzIo Token
+  /// </summary>
+  public string Token { get; set; } = default!;
 
-    /// <summary>
-    /// LogzIo Region : us | eu
-    /// </summary>
-    public string Region { get; set; } = default!;
+  /// <summary>
+  /// LogzIo Region : us | eu
+  /// </summary>
+  public string Region { get; set; } = default!;
 }
