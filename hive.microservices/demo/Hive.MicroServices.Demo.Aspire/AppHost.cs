d175eacc15abcfa37a0f543bce32e2b0c586c50a
Maciek Misztal <mmisztal1980@gmail.com>
2025-12-14 19:02:25 +0100

    chore: sdk updated to beta.5 . hive.microservices.demo.api added to aspire

--- hive.microservices/demo/Hive.MicroServices.Demo.Aspire/AppHost.cs
+++ hive.microservices/demo/Hive.MicroServices.Demo.Aspire/AppHost.cs
@@ -1,3 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.Hive_MicroServices_Demo_Api>("hive-microservices-demo-api");
+
+/*
+ var apiService = builder.AddProject<Projects.PoC_ApiService>("apiservice")
+       .WithHttpHealthCheck("/health");
+
+   builder.AddProject<Projects.PoC_Web>("webfrontend")
+       .WithExternalHttpEndpoints()
+       .WithHttpHealthCheck("/health")
+       .WithReference(apiService)
+       .WaitFor(apiService);
+ */
+
 builder.Build().Run();
